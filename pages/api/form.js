0dd73d9067599efabbd305a53afca16751ea1e0a
Clinscott <clinscott@gmail.com>
2022-11-10 13:06:06 -0500

    logic update

--- pages/api/form.js
+++ pages/api/form.js
@@ -25,8 +25,10 @@ export default async function handler(req, res){
     // Found the website.
     // Sends a HTTP success code
     res.status(200).json({ data: `${body.website}` });
+
     // lints website removing http... and zone for searching
     const website = websiteLinter(body.website);
+    
     // hashes searched website to match against parsed data. Searching for hashes which point to
     // the location of the searched website. Why. Because I wanted to learn how to.
     const hashedWebsite = await toHash(website.site);
--- pages/components/websiteNameSearcher.js
+++ pages/components/websiteNameSearcher.js
@@ -7,7 +7,28 @@
 //no consecutive -'s
 //not case sensitive
 
+export default async function websiteNameSearcher(str) {
+  //check if str has been searched before. if not then go nuts.
+  //otherwise return already created variables and perform WHOIS.
+  //add -com
+  //add -org
+  //add -net
+  //add -ca
+  //add -gov
+  //add -official tags: ie. northammergames => northammergames-store etc...
+  //change o to 0's: northammergames => n0rthammergames
+  //change i to l
+  //change l to i
+  //double small letters i, l, o, j: ie, northammergames => noorthammergames
+  //add e to end: ie. northammergames => northammergamese
+  //add s to end: ie. northammergames => northammergamess
+  //add - starting after first index untill second last index: ie. northammergames => n-orthammergames / no-rthammergames / nor-thammergames
+  //add another - from str from above function where no - before or after: ie. northammergames => n-o-rthammergames etc...
+  //perform each prev variation on subsequent variations
 
-export default async function websiteNameSearcher(str){
+  
+  //once collected store all found variations in new database associated with search str.
+};
 
-}
+//should each of these be their own function? Most likely yes.
+//variation functions go below here!
