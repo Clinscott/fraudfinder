012a4886741c7ca300a3194f93b631f68fe60189
Clinscott <clinscott@gmail.com>
2022-11-15 11:54:15 -0500

    Update websiteNameSearcher.js
    working on functions

--- pages/components/websiteNameSearcher.js
+++ pages/components/websiteNameSearcher.js
@@ -10,25 +10,48 @@
 export default async function websiteNameSearcher(str) {
   //check if str has been searched before. if not then go nuts.
   //otherwise return already created variables and perform WHOIS.
-  //add -com
-  //add -org
-  //add -net
-  //add -ca
-  //add -gov
-  //add -official tags: ie. northammergames => northammergames-store etc...
-  //change o to 0's: northammergames => n0rthammergames
-  //change i to l
-  //change l to i
-  //double small letters i, l, o, j: ie, northammergames => noorthammergames
-  //add e to end: ie. northammergames => northammergamese
-  //add s to end: ie. northammergames => northammergamess
-  //add - starting after first index untill second last index: ie. northammergames => n-orthammergames / no-rthammergames / nor-thammergames
-  //add another - from str from above function where no - before or after: ie. northammergames => n-o-rthammergames etc...
-  //perform each prev variation on subsequent variations
-
-  
   //once collected store all found variations in new database associated with search str.
-};
+}
 
 //should each of these be their own function? Most likely yes.
 //variation functions go below here!
+
+//add -com
+function addCom(str) {
+  return str + "-com";
+}
+
+console.log(addCom("northammergames"));
+//add -org
+function addOrg(str) {
+  return str + "-org";
+}
+//add -net
+function addNet(str) {
+  return str + "-net";
+}
+//add -ca
+function addCa(str) {
+  return str + "-ca";
+}
+//add -gov
+function addGov(str) {
+  return str + "-gov";
+}
+//add -official tags: ie. northammergames => northammergames-store etc...
+function addStore(str){
+  return str + '-store';
+}
+function addShop(str){
+  return str + '-shop';
+}
+//change o to 0's: northammergames => n0rthammergames
+
+//change i to l
+//change l to i
+//double small letters i, l, o, j: ie, northammergames => noorthammergames
+//add e to end: ie. northammergames => northammergamese
+//add s to end: ie. northammergames => northammergamess
+//add - starting after first index untill second last index: ie. northammergames => n-orthammergames / no-rthammergames / nor-thammergames
+//add another - from str from above function where no - before or after: ie. northammergames => n-o-rthammergames etc...
+//perform each prev variation on subsequent variations
